5109f7880fb78cc06e599e44a371865b86efe820
zsv <zsv@rtp3.ru>
2024-07-08 09:40:19 +0300

    08/07/2004 Review

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,31 +1,31 @@
 const classOpened = 'popup_is-opened';
 const classBtnClosed = '.popup__close';
 
-let dlgOpened;
+let OpenedDialog;
 
 export function openModal(dlg) {
-  dlgOpened=dlg;
+  OpenedDialog=dlg;
   dlg.classList.add(classOpened);
-  dlg.addEventListener('keydown', dialogCloseByEscape);
-  dlg.addEventListener('click', dialogCloseByOverlay);
-  dlg.querySelector(classBtnClosed).addEventListener('click',dialogCloseByButton);
+  dlg.addEventListener('keydown', CloseDialogByEscape);
+  dlg.addEventListener('click', CloseDialogByOverlay);
+  dlg.querySelector(classBtnClosed).addEventListener('click',CloseDialogByButton);
 }
 
 export function closeModal(dlg) {
-	dlg.removeEventListener('keydown', dialogCloseByEscape);
-	dlg.removeEventListener('click', dialogCloseByOverlay);
-  dlg.querySelector(classBtnClosed).removeEventListener('click',dialogCloseByButton);
+  dlg.removeEventListener('keydown', CloseDialogByEscape);
+  dlg.removeEventListener('click', CloseDialogByOverlay);
+  dlg.querySelector(classBtnClosed).removeEventListener('click',CloseDialogByButton);
   dlg.classList.remove(classOpened);
 }
 
-function dialogCloseByButton() {
-  closeModal(dlgOpened);
+function CloseDialogByButton() {
+  closeModal(OpenedDialog);
 }
 
-function dialogCloseByEscape(evt) {
-  if (evt.key === 'Escape') closeModal(dlgOpened);
+function CloseDialogByEscape(evt) {
+  if (evt.key === 'Escape') closeModal(OpenedDialog);
 }
 
-function dialogCloseByOverlay(evt) {
-  if (evt.target === dlgOpened) closeModal(dlgOpened);
+function CloseDialogByOverlay(evt) {
+  if (evt.target === OpenedDialog) closeModal(OpenedDialog);
 }
