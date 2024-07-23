d50e9d4e6cdd69d35fafe00a592cb631ad8f5eb4
Sergey <zsv@rtp3.ru>
2024-07-23 21:30:14 +0300

    23/07/2004 request

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -8,15 +8,23 @@ const config = {
   }
 }
 
+function request(url, options) {
+  return fetch(url, options).then(checkResponse)
+}
+
+function checkResponse(res) {
+  if (res.ok) return res.json()
+  else return Promise.reject(`Ошибка ${res.status}`);
+}
+
 export const getMyProfile = () => {
-  return fetch(`${config.baseUrl}users/me`, {
+  return request(`${config.baseUrl}users/me`, {
     headers: config.headers
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const setMyProfile = (name, about) => {
-  return fetch(`${config.baseUrl}users/me`, {
+  return request(`${config.baseUrl}users/me`, {
     method: 'PATCH',
     headers: config.headers,
     body: JSON.stringify({
@@ -24,62 +32,50 @@ export const setMyProfile = (name, about) => {
       about: about,
     })
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const setMyAvatar = (avatar) => {
-  return fetch(`${config.baseUrl}users/me/avatar`, {
+  return request(`${config.baseUrl}users/me/avatar`, {
     method: 'PATCH',
     headers: config.headers,
     body: JSON.stringify({
     avatar: avatar
     })
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const getInitialCards = () => {
-  return fetch(`${config.baseUrl}cards`, {
+  return request(`${config.baseUrl}cards`, {
     headers: config.headers
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const setLike = (card) => {
-  return fetch(`${config.baseUrl}/cards/likes/${card.id}`, {
+  return request(`${config.baseUrl}/cards/likes/${card.id}`, {
     method: 'PUT',
     headers: config.headers
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const unsetLike = (card) => {
-  return fetch(`${config.baseUrl}/cards/likes/${card.id}`, {
+  return request(`${config.baseUrl}/cards/likes/${card.id}`, {
     method: 'DELETE',
     headers: config.headers
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const postNewCard = (card) => {
-  return fetch(`${config.baseUrl}cards`, {
+  return request(`${config.baseUrl}cards`, {
     method: 'POST',
     headers: config.headers,
     body: JSON.stringify(card)
   })
-  .then((res) =>checkResponse(res));
 }
 
 export const deleteCard = (card) => {
-  return fetch(`${config.baseUrl}cards/${card.id}`, {
+  return request(`${config.baseUrl}cards/${card.id}`, {
     method: 'DELETE',
     headers: config.headers,
     body: JSON.stringify(card)
   })
-  .then((res) =>checkResponse(res));
-}
-
-function checkResponse(res) {
-  if (res.ok) return res.json()
-  else return Promise.reject(`Ошибка ${res.status}`);
 }
